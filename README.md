b137ff60888917093dd53654f232a1f8700bdc0a
Martin Angelov <martin@tutorialzine.com>
2014-11-19 18:20:07 +0200

    Fixed a possible security issue with sandbox requests. Added tests. Closes #11

--- README.md
+++ README.md
@@ -11,11 +11,12 @@ $ npm install paypal-ipn
 There is only one function, `verify`, which is used to verify any IPN messages you receive:
 
 ```javascript
-ipn.verify(ipn_params, callback);
+ipn.verify(ipn_params, options, callback);
 ```
 
 `ipn_params` is the dictionary of POST values sent to your IPN script by PayPal. Don't modify the dict in any way, just pass it directly to `ipn.verify` to check if the IPN message is valid.
 
+`options` is an optional object, which may contain the property `sandbox`. If it is set to `true`, the verify function will use PayPal's Sandbox URL for validation. Use this *only* for development, when testing requests with the [PayPal Simulator](https://developer.paypal.com/webapps/developer/applications/ipn_simulator).
 
 Example code:
 
@@ -38,6 +39,26 @@ ipn.verify(params, function callback(err, msg) {
 });
 ```
 
+While testing with the simulator, you may pass `{sandbox: true}` as the options object:
+
+```javascript
+
+ipn.verify(params, {sandbox: true}, function callback(err, msg) {
+  if (err) {
+    console.error(err);
+  } else {
+    // Do stuff with original params here
+
+    if (params.payment_status == 'Completed') {
+      // Payment has been confirmed as completed
+    }
+  }
+});
+```
+
+Alternatively, you may set the sandbox to true only in development like this: `{ sandbox: (process.env.NODE_ENV == 'development') }`.
+
+### Additional checks
 Note that all the package does is confirm that the IPN message is valid. After this, you will still need to make some more checks:
 
 * Confirm that the `payment_status` is `Completed`.
@@ -62,7 +83,7 @@ In case IPN was invalid or the http request failed `err` holds the Error object.
 
 All you need to do is pass in the request parameters to `ipn.verify`.
 
-In ExpressJS, the request parameters are in `req.body`:
+If you use the [body parser](https://github.com/expressjs/body-parser) middleware in ExpressJS, the request parameters are available in `req.body`:
 
 ```javascript
 ipn.verify(req.body, callback_function);
--- tests/ipn.js
+++ tests/ipn.js
@@ -1,11 +1,13 @@
 var ipn = require('../index');
 var qs = require('querystring');
+var rewire = require('rewire');
+
+var ipn_sandbox_msg = 'mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&payer_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-1252&address_zip=95131&first_name=Test&mc_fee=0.88&address_country_code=US&address_name=Test+User&notify_version=2.6&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=AtkOfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-1XcmSoGtf&payer_email=gpmac_1231902590_per%40paypal.com&txn_id=61E67681CH3238416&payment_type=instant&last_name=User&address_state=CA&receiver_email=gpmac_1231902686_biz%40paypal.com&payment_fee=0.88&receiver_id=S8XGHLYDW9T3S&txn_type=express_checkout&item_name=&mc_currency=USD&item_number=&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=19.95&shipping=0.00';
 
 // tests for bad (fake) IPN requests
 module.exports['Fake IPN request'] = function(test) {
-  var message = 'mc_gross=19.95&protection_eligibility=Eligible&address_status=confirmed&payer_id=LPLWNMTBWMFAY&tax=0.00&address_street=1+Main+St&payment_date=20%3A12%3A59+Jan+13%2C+2009+PST&payment_status=Completed&charset=windows-1252&address_zip=95131&first_name=Test&mc_fee=0.88&address_country_code=US&address_name=Test+User&notify_version=2.6&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=AtkOfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-1XcmSoGtf&payer_email=gpmac_1231902590_per%40paypal.com&txn_id=61E67681CH3238416&payment_type=instant&last_name=User&address_state=CA&receiver_email=gpmac_1231902686_biz%40paypal.com&payment_fee=0.88&receiver_id=S8XGHLYDW9T3S&txn_type=express_checkout&item_name=&mc_currency=USD&item_number=&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=19.95&shipping=0.00';
 
-  var params = qs.parse(message);
+  var params = qs.parse(ipn_sandbox_msg);
 
   ipn.verify(params, function callback(err, msg) {
     test.notEqual(err, null, 'when returning error, err should not equal null');
@@ -16,9 +18,93 @@ module.exports['Fake IPN request'] = function(test) {
 
 // test for undefined parameters passed to function
 module.exports['Undefined parameters'] = function(test) {
-	ipn.verify(undefined, function(err, msg) {
-		test.notEqual(err, null, 'when returning error on undefined parameters, err should not equal null');
+  ipn.verify(undefined, function(err, msg) {
+    test.notEqual(err, null, 'when returning error on undefined parameters, err should not equal null');
 
-		test.done();
-	});
+    test.done();
+  });
+};
+
+// test for simulated requests while sandbox is false
+module.exports['test_ipn with sandbox:false'] = function(test){
+
+  var params = qs.parse(ipn_sandbox_msg);
+
+  ipn.verify(params, { sandbox:false }, function(e){
+    test.equal(e.message, 'Sandbox request received, but the sandbox option is not set');
+
+    test.done();
+  });
+
+};
+
+// test for which url is requested when sandbox is true
+module.exports['URL when sandbox is true'] = function(test){
+
+  var ipn = rewire('../lib/paypal-ipn.js');
+
+  var fake_https = {}, f = function(){}, 
+      req = { write: f, end: f, on:f };
+
+  var params = qs.parse(ipn_sandbox_msg);
+
+
+  fake_https.request = function(options, callback){
+    test.equal(options.host, 'www.sandbox.paypal.com', 'when sandbox is true, www.sandbox.paypal.com should be used');
+    test.done();
+    return req;
+  };
+
+  ipn.__set__('https', fake_https);
+
+  ipn.verify(params, { sandbox:true }, function(){});
+
+};
+
+// test for which url is requested when sandbox is false
+module.exports['URL when sandbox is false'] = function(test){
+
+  var ipn = rewire('../lib/paypal-ipn.js');
+
+  var fake_https = {}, f = function(){}, 
+      req = { write: f, end: f, on:f };
+
+  var params = qs.parse(ipn_sandbox_msg);
+
+  delete params.test_ipn;
+
+  fake_https.request = function(options, callback){
+    test.equal(options.host, 'www.paypal.com', 'when sandbox is false, www.paypal.com should be used');
+    test.done();
+    return req;
+  };
+
+  ipn.__set__('https', fake_https);
+
+  ipn.verify(params, { sandbox:false }, function(){});
+
+};
+
+// test for which url is requested when sandbox is not set
+module.exports['URL when sandbox is undefined'] = function(test){
+
+  var ipn = rewire('../lib/paypal-ipn.js');
+
+  var fake_https = {}, f = function(){}, 
+      req = { write: f, end: f, on:f };
+
+  var params = qs.parse(ipn_sandbox_msg);
+
+  delete params.test_ipn;
+
+  fake_https.request = function(options, callback){
+    test.equal(options.host, 'www.paypal.com', 'when sandbox is undefined, www.paypal.com should be used');
+    test.done();
+    return req;
+  };
+
+  ipn.__set__('https', fake_https);
+
+  ipn.verify(params, function(){});
+
 };
